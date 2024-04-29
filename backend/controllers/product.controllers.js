f9dafd59467bce3af6d0e6f7d7a3ad8ac6f84007
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-04-29 17:33:36 +0530

    add controllers and routes for product

---
+++ backend/controllers/product.controllers.js
@@ -0,0 +1,27 @@
+
+//1. get all products
+
+export const getAllProduct = async ()=>{
+
+}
+
+//2. add new product
+export const addNewProduct = async ()=>{
+
+}
+
+//3. get product detail
+export const getSingleProduct = async ()=>{
+
+}
+
+//4. update product
+export const updateProduct = async ()=>{
+
+}
+
+
+//5. delete product
+export const deleteProduct = async ()=>{
+
+}
