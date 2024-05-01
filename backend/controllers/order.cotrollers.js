1dda18f4ce3061a6a52e1b7cfd51f2463ddf282c
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-05-01 20:14:49 +0530

    add admin routes and controllers to get all orders and update order status

--- backend/controllers/order.cotrollers.js
+++ backend/controllers/order.cotrollers.js
@@ -1,4 +1,5 @@
 import Order from "../models/order.model.js";
+import Product from "../models/product.models.js";
 import apiError from "../utils/apiError.js";
 import apiResponse from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
@@ -64,3 +65,52 @@ export const getOrderDetails = asyncHandler(async (req, res, next) => {
         new apiResponse(200, "order details fetched!", order)
     );
   });
+
+  // Get all orders - ADMIN  =>  /api/v1/admin/orders
+export const allOrders = asyncHandler(async (req, res, next) => {
+    const orders = await Order.find();
+  
+    return res
+    .status(200)
+    .json(
+        new apiResponse(200, "all order fetched!", orders)
+    );
+  });
+  
+  // Update Order - ADMIN  =>  /api/v1/admin/orders/:id
+  export const updateOrder = asyncHandler(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+  
+    if (!order) {
+      throw new apiError(404, "No Order found with this ID");
+    }
+  
+    if (order?.orderStatus === "Delivered") {
+      throw new apiError(404, "This order has already been delivered!");
+    }
+  
+    // Update products stock
+    order?.orderItems?.forEach(async (item) => {
+      const product = await Product.findById(item?.product?.toString());
+      if (!product) {
+        throw new apiError(404, "No Product found with this ID");
+      }
+      product.stock = product.stock - item.quantity;
+      await product.save({ validateBeforeSave: false });
+    });
+  
+    order.orderStatus = req.body.status;
+    order.deliveredAt = Date.now();
+  
+    await order
+        .save()
+        .then((udpatedOrder)=>{
+            return res
+            .status(200)
+            .json(
+                apiResponse(200, "order udpated!", updatedOrder)
+            );
+        })
+        .catch(error=>next(error));  
+  });
+  
--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,16 +1,20 @@
 import express from 'express'
 import { 
+    allOrders,
     getOrderDetails,
     myOrders,
-    newOrder 
+    newOrder, 
+    updateOrder
 } from '../controllers/order.cotrollers.js';
 import { verifyUser } from '../middlewares/auth.middlewares.js';
+import { verify } from 'crypto';
 
 const router = express.Router();
 
 router.route('/new-order').post(verifyUser, newOrder);
-router.route('/order-detail').get(verifyUser, myOrders);
-router.route('/order-details/:orderId').get(verifyUser, getOrderDetails);
-
+router.route('/me/order-details').get(verifyUser, myOrders);
+router.route('/order-detail/:orderId').get(verifyUser, getOrderDetails);
+router.route('/admin/get-all-orders').get(verifyUser, allOrders);
+router.route('/admin/update-order/:orderId').post(verifyUser, updateOrder);
 
 export default router;
